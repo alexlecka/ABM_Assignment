31ffba160a8ab5e0e37e65d402175d1b1d69ba94
alexlecka <alexandra.lecka@hotmail.sk>
2021-12-16 19:56:14 +0100

    Getting the swivles

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-# from mesa import Agent 
+import seaborn as sns
 import numpy as np
 import random
 
@@ -13,40 +13,54 @@ class Household():
         self.perception = perception
         self.knowledge = knowledge
         
-    def determine_multiplier(self, h_type):
+    def starting_val(self, h_type):
+        base = 40
         if h_type == 'individual':
-            return 1
+            return 1*base + random.gauss(5, 3)
         elif h_type == 'retired':
-            return 0.8
+            return 0.8*base + random.gauss(5, 3)
+        # because household type 'retired' is supposed to have the least waste
         elif h_type == 'family':
-            return 1.4
+            return 1.4*base + random.gauss(5, 3) 
+        # household type 'family' is supposed to have the most waste
         elif h_type == 'couple':
-            return 1.2
+            return 1.2*base + random.gauss(5, 3)
         else:
             print('Do not know this household type.')
             
-    def base_waste(self, year, std_fraction = 0.1):
-        waste = 40 - 0.04*year - np.exp(-0.01*year)*np.sin(0.3*year)
-        mean = waste*self.determine_multiplier(self.type)
-        return np.random.normal(mean, abs(mean*std_fraction))
+    def calc_starting_val(self):
+        self.starting_value = self.starting_val(self.type)
+            
+    def calc_base_waste(self, t):
+        waste = self.starting_value - 0.04*t - np.exp(-0.01*t)*np.sin(0.3*t)
+        self.base_waste = waste
     
-    def plastic_waste_assign(self, year):
+    def calc_plastic_waste(self, t):
         # perception as we use it here is equal to plastic fraction
-        self.plastic_waste = self.base_waste(year)*self.perception 
+        self.plastic_waste = self.base_waste*self.perception 
     
 # create n_households Household objects and assign a base and plastic waste to each of them    
     
 n_households = 50
-year_range = 200
+time_range = 240
 household_types = ['individual', 'retired', 'family', 'couple']
 household_list = []
+base_waste_dict = {}
 plastic_waste_dict = {}
+
+random.seed(3)
+
 for i in range(n_households):
+    base_waste_dict[str(i)] = []
     plastic_waste_dict[str(i)] = []
 for i in range(n_households):
     household_list.append(Household(random.choice(household_types), np.random.uniform(0, 1), np.random.uniform(0, 1)))
-    for yr in range(year_range):
-        household_list[-1].plastic_waste_assign(yr)
+    household_list[-1].calc_starting_val()
+    for t in range(time_range):
+        household_list[-1].calc_base_waste(t)
+        household_list[-1].calc_plastic_waste(t)
+        
+        base_waste_dict[str(i)].append(household_list[-1].base_waste)
         plastic_waste_dict[str(i)].append(household_list[-1].plastic_waste)
         
 #%% color assignment for visualization below
@@ -62,6 +76,8 @@ def assign_color(household_type):
         return 'yellow'
     
 #%% household type distribution
+
+sns.set_theme()
 
 types = []
 for household in household_list:
@@ -81,7 +97,7 @@ from matplotlib.lines import Line2D
     
 fig, ax = plt.subplots(1, figsize = (12, 9))
 for i in range(n_households):
-    ax.plot(np.arange(0, year_range, step = 1), plastic_waste_dict[str(i)], linewidth = 0.5, 
+    ax.plot(np.arange(0, time_range, step = 1), base_waste_dict[str(i)], linewidth = 0.5, 
             c = assign_color(household_list[i].type))
     
 custom_lines = [Line2D([0], [0], color = 'red', linewidth = 0.5),
@@ -90,7 +106,7 @@ custom_lines = [Line2D([0], [0], color = 'red', linewidth = 0.5),
                 Line2D([0], [0], color = 'yellow', linewidth = 0.5)]
 
 ax.set_xlabel('household')
-ax.set_ylabel('plastic waste (kg)')
+ax.set_ylabel('base waste (kg)')
 ax.legend(custom_lines, ['individual', 'retired', 'family', 'couple'], loc = 'best')
 plt.savefig('images/household_waste.png', bbox_inches = 'tight')
 plt.show()
