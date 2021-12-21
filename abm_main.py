7bb205802963d7c94703be325ef19d258e781fc2
alexlecka <alexandra.lecka@hotmail.sk>
2021-12-22 00:21:33 +0100

    household agent changes finalized (ish)

--- abm_main.py
+++ abm_main.py
@@ -8,3 +8,21 @@ from mesa.time import RandomActivation
 from mesa import Model, Agent
 from mesa.space import Grid
 
+class HouseholdRecycling(Model):
+    def __init__(self, n_households = 100):
+        
+        # initialize households as described in Drive
+        household_list = []
+        for i in range(n_households):
+            if i < 0.39*n_households:
+                household_list.append(Household('one_person', 0.5, 0.5))
+            else:
+                household_list.append(Household('multi_person', 0.5, 0.5))
+            household_list[-1].calc_starting_val()
+            
+        # initialize municipalities
+        
+        # initialize recycling companies 
+        
+    def step(self):
+        # here goes the main step function of the entire model 
--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mesa import Agent
 import seaborn as sns
 import numpy as np
 import random
@@ -6,7 +7,7 @@ import random
 #%% Household class - this part is the only one used for household agent, below 
 # are some examples and visualizations
 
-class Household():
+class Household(Agent):
     def __init__(self, household_type, perception, knowledge):
         # perception is separation, knowledge is how well we separate
         self.type = household_type
@@ -15,16 +16,10 @@ class Household():
         
     def starting_val(self, h_type):
         base = 40
-        if h_type == 'individual':
-            return 1*base + random.gauss(5, 3)
-        elif h_type == 'retired':
-            return 0.8*base + random.gauss(5, 3)
-        # because household type 'retired' is supposed to have the least waste
-        elif h_type == 'family':
-            return 1.4*base + random.gauss(5, 3) 
-        # household type 'family' is supposed to have the most waste
-        elif h_type == 'couple':
-            return 1.2*base + random.gauss(5, 3)
+        if h_type == 'one_person':
+            return 1*base
+        elif h_type == 'multi_person':
+            return 2.85*base
         else:
             print('Do not know this household type.')
             
@@ -41,9 +36,9 @@ class Household():
     
 # create n_households Household objects and assign a base and plastic waste to each of them    
     
-n_households = 50
+n_households = 100
 time_range = 240
-household_types = ['individual', 'retired', 'family', 'couple']
+household_types = ['one_person', 'multi_person']
 household_list = []
 base_waste_dict = {}
 plastic_waste_dict = {}
@@ -53,9 +48,15 @@ random.seed(3)
 for i in range(n_households):
     base_waste_dict[str(i)] = []
     plastic_waste_dict[str(i)] = []
+    
 for i in range(n_households):
-    household_list.append(Household(random.choice(household_types), np.random.uniform(0, 1), np.random.uniform(0, 1)))
+    if i < 0.39*n_households:
+        household_list.append(Household('one_person', 0.5, 0.5))
+    else:
+        household_list.append(Household('multi_person', 0.5, 0.5))
+        
     household_list[-1].calc_starting_val()
+    
     for t in range(time_range):
         household_list[-1].calc_base_waste(t)
         household_list[-1].calc_plastic_waste(t)
@@ -66,14 +67,10 @@ for i in range(n_households):
 #%% color assignment for visualization below
         
 def assign_color(household_type):
-    if household_type == 'individual':
+    if household_type == 'one_person':
         return 'red'
-    elif household_type == 'retired':
+    elif household_type == 'multi_person':
         return 'blue'
-    elif household_type == 'family':
-        return 'green'
-    elif household_type == 'couple':
-        return 'yellow'
     
 #%% household type distribution
 
@@ -82,10 +79,9 @@ sns.set_theme()
 types = []
 for household in household_list:
     types.append(household.type)
-heights = [types.count(household_types[0]), types.count(household_types[1]),
-           types.count(household_types[2]), types.count(household_types[3])]
+heights = [types.count(household_types[0]), types.count(household_types[1])]
 fig, ax = plt.subplots(1)
-ax.bar(np.arange(0, len(household_types)), heights, align = 'center', color = ['red', 'blue', 'green', 'yellow'])
+ax.bar(np.arange(0, len(household_types)), heights, align = 'center', color = ['red', 'blue'])
 plt.xticks(np.arange(0, len(household_types)), household_types)
 plt.savefig('images/household_type_distribution.png', bbox_inches = 'tight')
 plt.show()
@@ -101,13 +97,11 @@ for i in range(n_households):
             c = assign_color(household_list[i].type))
     
 custom_lines = [Line2D([0], [0], color = 'red', linewidth = 0.5),
-                Line2D([0], [0], color = 'blue', linewidth = 0.5),
-                Line2D([0], [0], color = 'green', linewidth = 0.5),
-                Line2D([0], [0], color = 'yellow', linewidth = 0.5)]
+                Line2D([0], [0], color = 'blue', linewidth = 0.5)]
 
 ax.set_xlabel('household')
 ax.set_ylabel('base waste (kg)')
-ax.legend(custom_lines, ['individual', 'retired', 'family', 'couple'], loc = 'best')
+ax.legend(custom_lines, ['one_person', 'multi_person'], loc = 'best')
 plt.savefig('images/household_waste.png', bbox_inches = 'tight')
 plt.show()
 plt.close()
