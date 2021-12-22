1a7d72d740ac4c6932dc76060cc1b2edda1add55
alexlecka <alexandra.lecka@hotmail.sk>
2021-12-22 20:34:28 +0100

    Updating Household class agent

--- Household.py
+++ Household.py
@@ -1,100 +1,107 @@
 from mesa import Agent
+import numpy as np
 
 class Household(Agent):
-    def __init__(self, unique_id, object_reference_municipality, model, household_type, perception, knowledge):
-        super().__init__(unique_id, model)
+    def __init__(self, household_type, perception, knowledge):
         # perception is separation, knowledge is how well we separate
-        self.id = unique_id  # unique id of Household example M1_H1 for Municipality 1 household 1
         self.type = household_type
         self.perception = perception
         self.knowledge = knowledge
-        self.municipality = object_reference_municipality
-
-    def determine_multiplier(self, h_type):
-        if h_type == 'individual':
-            return 1
-        elif h_type == 'retired':
-            return 0.8
-        elif h_type == 'family':
-            return 1.4
-        elif h_type == 'couple':
-            return 1.2
+        
+    def starting_val(self, h_type):
+        base = 40 # 
+        if h_type == 'one_person':
+            return 1*base
+        elif h_type == 'multi_person':
+            return 2.85*base
         else:
             print('Do not know this household type.')
-
-    def base_waste(self, year, std_fraction=0.1):
-        waste = 40 - 0.04 * year - np.exp(-0.01 * year) * np.sin(0.3 * year)
-        mean = waste * self.determine_multiplier(self.type)
-        return np.random.normal(mean, abs(mean * std_fraction))
-
-    def plastic_waste_assign(self, year):
+            
+    def calc_starting_val(self):
+        self.starting_value = self.starting_val(self.type)
+            
+    def calc_base_waste(self, t):
+        waste = self.starting_value - 0.04*t - np.exp(-0.01*t)*np.sin(0.3*t)
+        self.base_waste = waste
+    
+    def calc_plastic_waste(self, t):
         # perception as we use it here is equal to plastic fraction
-        self.plastic_waste = self.base_waste(year) * self.perception
+        self.plastic_waste = self.base_waste*self.perception 
 
     # Overriding of __str__ to get some useful information when calling print on household
     def __str__(self):
         return 'House id: {}'.format(self.id)
+    
+#%%
 
-# %%
-# n_households = 50
-# year_range = 200
-# household_types = ['individual', 'retired', 'family', 'couple']
+# create n_households Household objects and assign a base and plastic waste to each of them    
+    
+# n_households = 100
+# time_range = 240
+# household_types = ['one_person', 'multi_person']
 # household_list = []
+# base_waste_dict = {}
 # plastic_waste_dict = {}
-# for i in range(n_households):
-#     plastic_waste_dict[str(i)] = []
-# for i in range(n_households):
-#     household_list.append(Household(random.choice(household_types), np.random.uniform(0, 1),
-#                                     np.random.uniform(0, 1), i))
-#     for yr in range(year_range):
-#         household_list[-1].plastic_waste_assign(yr)
-#         plastic_waste_dict[str(i)].append(household_list[-1].plastic_waste)
 
-# #%%
-#
+# random.seed(3)
+
+# for i in range(n_households):
+#     base_waste_dict[str(i)] = []
+#     plastic_waste_dict[str(i)] = []
+    
+# for i in range(n_households):
+#     if i < 0.39*n_households:
+#         household_list.append(Household('one_person', 0.5, 0.5))
+#     else:
+#         household_list.append(Household('multi_person', 0.5, 0.5))
+        
+#     household_list[-1].calc_starting_val()
+    
+#     for t in range(time_range):
+#         household_list[-1].calc_base_waste(t)
+#         household_list[-1].calc_plastic_waste(t)
+        
+#         base_waste_dict[str(i)].append(household_list[-1].base_waste)
+#         plastic_waste_dict[str(i)].append(household_list[-1].plastic_waste)
+        
+#%% color assignment for visualization below
+        
 # def assign_color(household_type):
-#     if household_type == 'individual':
+#     if household_type == 'one_person':
 #         return 'red'
-#     elif household_type == 'retired':
+#     elif household_type == 'multi_person':
 #         return 'blue'
-#     elif household_type == 'family':
-#         return 'green'
-#     elif household_type == 'couple':
-#         return 'yellow'
-#
-# #%%
-#
+    
+#%% household type distribution
+
+# sns.set_theme()
+
 # types = []
 # for household in household_list:
 #     types.append(household.type)
-#
-# heights = [types.count(household_types[0]), types.count(household_types[1]),
-#            types.count(household_types[2]), types.count(household_types[3])]
+# heights = [types.count(household_types[0]), types.count(household_types[1])]
 # fig, ax = plt.subplots(1)
-# ax.bar(np.arange(0, len(household_types)), heights, align = 'center')
+# ax.bar(np.arange(0, len(household_types)), heights, align = 'center', color = ['red', 'blue'])
 # plt.xticks(np.arange(0, len(household_types)), household_types)
 # plt.savefig('images/household_type_distribution.png', bbox_inches = 'tight')
 # plt.show()
 # plt.close()
-#
-# #%%
-#
+
+#%% plastic waste over time for all households
+
 # from matplotlib.lines import Line2D
-#
+    
 # fig, ax = plt.subplots(1, figsize = (12, 9))
 # for i in range(n_households):
-#     ax.plot(np.arange(0, year_range, step = 1), plastic_waste_dict[str(i)], linewidth = 0.5,
+#     ax.plot(np.arange(0, time_range, step = 1), base_waste_dict[str(i)], linewidth = 0.5, 
 #             c = assign_color(household_list[i].type))
-#
+    
 # custom_lines = [Line2D([0], [0], color = 'red', linewidth = 0.5),
-#                 Line2D([0], [0], color = 'blue', linewidth = 0.5),
-#                 Line2D([0], [0], color = 'green', linewidth = 0.5),
-#                 Line2D([0], [0], color = 'black', linewidth = 0.5)]
-#
+#                 Line2D([0], [0], color = 'blue', linewidth = 0.5)]
+
 # ax.set_xlabel('household')
-# ax.set_ylabel('plastic waste (kg)')
-# ax.legend(custom_lines, ['individual', 'retired', 'family', 'couple'], loc = 'best')
+# ax.set_ylabel('base waste (kg)')
+# ax.legend(custom_lines, ['one_person', 'multi_person'], loc = 'best')
 # plt.savefig('images/household_waste.png', bbox_inches = 'tight')
 # plt.show()
 # plt.close()
-
