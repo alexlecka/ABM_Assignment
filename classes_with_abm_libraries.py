e217f2dd6908bcf0abbe0721fdacf1017098100e
haristh <haristheodorou8@hotmail.com>
2021-12-22 08:59:43 +0200

    Update classes_with_abm_libraries.py
    adding Model and Agent. Getting a histogram for final budgets

--- classes_with_abm_libraries.py
+++ classes_with_abm_libraries.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from mesa import Agent, Model
+from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 #load all available schedulers
 import mesa.time as time
@@ -109,8 +110,9 @@ plt.close()
 # we want technologies that improve the efficiency of recycling plastics, for 
 # simplifiation any extra technology improves efficiency and there is no overlap for now
 
-class RecyclingCompany:
-    def __init__(self, init_money = 1000, init_efficiency = 0.1, price=50):
+class RecyclingCompany(Agent):
+    def __init__(self, unique_id, model, init_money = 1000, init_efficiency = 0.1, price=50):
+        super().__init__(unique_id, model)
 #        self.budget = random.randrange(init_money) #0-1000 
         self.budget = init_money
         self.efficiency = init_efficiency
@@ -134,8 +136,31 @@ class RecyclingCompany:
                     self.all_tech= self.all_tech[:i]+self.all_tech[i+1:]
                     
                     break
+    def step(self):
+        self.new_tech()
 
+class Model(Model):
+    """A model with some number of agents."""
+    def __init__(self, N):
+        self.num_recycling_companies = N
+        self.schedule = RandomActivation(self)
+        
+        # Create agents
+        for i in range(self.num_recycling_companies):
+            a = RecyclingCompany(i, self)
+            self.schedule.add(a)
+    
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
 
+model = Model(10)
+for i in range(20):
+    model.step()
+    
+company_budget = [a.budget for a in model.schedule.agents]
+plt.hist(company_budget)
+plt.show()
 
 #%%
 n_companies = 10
